92766384ed576eed342b53cee21b95564c0fce1e
Egil Moeller <egil.moller@freecode.no>
2010-03-13 11:24:20 +0100

    Bugfixed the kafoo test-plugin so that it doesn't generate copious amounts of debug info

--- trunk/etherpad/src/plugins/kafoo/main.js
+++ trunk/etherpad/src/plugins/kafoo/main.js
@@ -1,8 +1,16 @@
-hooks = ['nahook', 'fiehook'];
-description = 'Kabar';
+import("etherpad.log");
+
+function init() {
+ this.hooks = [];
+ this.description = 'KaBar plugin';
+ this.install = install;
+ this.uninstall = uninstall;
+}
 
 function install() {
+ log.info("Installing testplugin");
 }
 
 function uninstall() {
+ log.info("Uninstalling testplugin");
 }
