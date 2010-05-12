c10692139b39913ea76e44e6b3ed1246e17598e9
Mikko Rantalainen <mikko.rantalainen@peda.net>
2010-05-12 16:29:23 +0300

    Added more logic to keep openoffice.org process under control
    Patch includes an LGPL licensed python script to bring up openoffice,
    optionally convert between formats from command line and shut down
    the openoffice process.

--- bin/run.sh
+++ bin/run.sh
@@ -10,12 +10,8 @@ export JAVA="/usr/lib/jvm/java-6-sun/bin/java"
 export SCALA="/usr/bin/scala"
 export PATH="/usr/lib/jvm/java-6-sun/bin:$PATH"
 export LOG="/var/log/etherpad/server.log"
-export SOFFICE_BIN="/usr/bin/soffice"
 #####
 
 cd etherpad
-if [ -x "$SOFFICE_BIN" ]; then
-	"$SOFFICE_BIN" -headless -nofirststartwizard -accept="socket,host=localhost,port=8100;urp;StarOffice.Service" &
-fi
 exec bin/run-local.sh --etherpad.soffice="$SOFFICE_BIN" "$@" >> "$LOG" 2>&1
 
