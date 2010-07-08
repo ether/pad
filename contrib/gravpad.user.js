73ab7a1aad70ca4a6a0f94653060cdf993470246
Joe Corneli <holtzermann17@gmail.com>
2010-07-09 00:26:45 +0200

    Updated gravpad.user.js to use real-time recent changes, and also so it only runs on the top iframe

--- contrib/gravpad.user.js
+++ contrib/gravpad.user.js
@@ -3,10 +3,11 @@
 // @namespace     http://metameso.org/
 // @description   An iframe appears on every page, containing an etherpad!
 // @exclude       *metameso.org:9000*
-// @exclude       *doc.etherpad.org*
 // @exclude       about:blank
 // @version       0.1
 // ==/UserScript==
+
+if (window != top) return; // <-- suggested as a way to prevent running on sub-iframes.
 
 // It seems necessary to exclude most versions of
 // Etherpad, apparently because an extra iframe triggers
@@ -22,11 +23,20 @@ var newTop = document.createElement("iframe");
 newTop.setAttribute("id", "addedPad");
 newTop.setAttribute("name", "addedPad");
 newTop.setAttribute("frameborder", "yes");
-newTop.setAttribute("height", "887px");
+newTop.setAttribute("height", "787px");
 newTop.setAttribute("width", "40%");
-newTop.setAttribute("style", "float: right; position: fixed; z-index: 9999; right: 1px;");
+newTop.setAttribute("style", "float: right; position: fixed; z-index: 999; right: 1px;");
 newTop.setAttribute("src", "http://metameso.org:9000/" +
                      window.location.href.replace(/\//gi,"-"));
 document.documentElement.appendChild(newTop);
 
 
+var rtrc = document.createElement("iframe");
+rtrc.setAttribute("id", "addedChanges");
+rtrc.setAttribute("name", "addedChanges");
+rtrc.setAttribute("frameborder", "yes");
+rtrc.setAttribute("height", "207px");
+rtrc.setAttribute("width", "40%");
+rtrc.setAttribute("style", "float: right; position: fixed; bottom: 0; fixed; z-index: 9999; right: 1px;");
+rtrc.setAttribute("src", "http://metameso.org:9000/ep/rtrc/");
+document.documentElement.appendChild(rtrc);
