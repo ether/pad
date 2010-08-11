835167b5050fae91d6e29b6ba110202891f96440
Michael Forrest <michael.forrest@gmail.com>
2010-08-11 14:37:14 +0100

    show groups of documents

--- etherpad/src/plugins/supportGroupUrls/controllers/groupUrls.js
+++ etherpad/src/plugins/supportGroupUrls/controllers/groupUrls.js
@@ -1,9 +1,23 @@
 import("etherpad.utils.*");
 import("etherpad.log");
 import("etherpad.control.pad.pad_control");
+import("sqlbase.sqlobj");
 function onRequest() {
-	var filter = request.path.toString().split("/specs/")[1].replace(/\//g,"-");
-	renderHtml('groupIndex.ejs',{filter:filter},'supportGroupUrls');
+	var section = request.path.toString().split("/specs/")[1];
+	var filter = section.replace(/\//g,"-");
+	function shortName(pad_id){
+		return pad_id.replace(filter, '');
+	}
+	function groupBasedId(pad_id){
+		return request.path + shortName(pad_id);
+	}
+	
+	var matching_pads = sqlobj.selectMulti("PAD_SQLMETA",{id:['like', filter+'%']});
+	renderHtml('groupIndex.ejs',{section:section,
+									filter:filter, 
+									pads:matching_pads,
+									groupBasedId:groupBasedId,
+									shortName:shortName},'supportGroupUrls');
 	return true;
 }
 function render_page(){
--- etherpad/src/plugins/supportGroupUrls/hooks.js
+++ etherpad/src/plugins/supportGroupUrls/hooks.js
@@ -2,7 +2,7 @@ import("etherpad.log");
 import("sqlbase.sqlbase");
 import("plugins.supportGroupUrls.controllers.groupUrls");
 import("dispatch.{Dispatcher,PrefixMatcher,forward}");
-import("etherpad.control.pad.pad_control");
+
 
 function handlePath() {
   return [
