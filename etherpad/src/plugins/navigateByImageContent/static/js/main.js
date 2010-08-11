88b08988c442dc6eef5c7e72a943004b6ac1fa40
Michael Forrest <michael.forrest@gmail.com>
2010-08-11 15:18:09 +0100

    bug fix

--- etherpad/src/plugins/navigateByImageContent/static/js/main.js
+++ etherpad/src/plugins/navigateByImageContent/static/js/main.js
@@ -9,14 +9,24 @@ function init() {
 
 function renderPageBodyPre(){
 	var result = '<table><tr>';
+	var query= {};
+	var getPath = function(id){return id;};
+	var match = request.path.match(/^((\/specs\/)([^\/]+\/)+)([^\/]+)$/);
+	if(match){ 
+		
+		var section = request.path.replace(match[2], '').replace(match[4], '');
+		var like = section.replace(/\//g,"-");
+		log.info("\n\nlike query = " + like + "\n" + match + " \n");
+		query = { id:['like', like + "%" ]  };
+		getPath = function(id){return match[1] + id.replace(like,'');};
+	}
 	
-	
-	var allPads = sqlobj.selectMulti("PAD_SQLMETA",{});
+	var allPads = sqlobj.selectMulti("PAD_SQLMETA",query);
 	
 	for(var i=0; i < allPads.length; i++){
 		var pad = allPads[i];
 		var meta = sqlbase.getJSON("PAD_META", pad.id);
-		result += '<th><a href="/' + pad.id + '" title="'+pad.id+'">';
+		result += '<th><a href="' + getPath(pad.id) + '" title="'+pad.id+'">';
 		if (meta && meta.images && meta.images.constructor && meta.images.constructor.toString().match(/Array/)) {
 			
 			result += '<img src="' + meta.images[0] + '" alt="' + pad.id + '" width="60px"/>';
