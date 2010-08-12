fdc22cf1150ef76154b047a4dbdb7397cd7c9d50
Michael Forrest <michael.forrest@gmail.com>
2010-08-12 14:09:10 +0100

    extracted the template for the image navigation

--- etherpad/src/plugins/navigateByImageContent/static/js/main.js
+++ etherpad/src/plugins/navigateByImageContent/static/js/main.js
@@ -1,7 +1,7 @@
 import("sqlbase.sqlobj");
 import("sqlbase.sqlbase");
 import("etherpad.log");
-
+import("etherpad.utils.*");
 function init() {
   this.hooks = ['renderNavigation',];
   this.aceGetFilterStack = renderNavigation;
@@ -9,31 +9,35 @@ function init() {
 
 function renderNavigation(){
 	var result = '<table><tr>';
+	var section;
 	var query= {};
 	var getPath = function(id){return id;};
 	var match = request.path.match(/^((\/specs\/)([^\/]+\/)+)([^\/]+)$/);
 	if(match){ 
-		
-		var section = request.path.replace(match[2], '').replace(match[4], '');
+		section = request.path.replace(match[2], '').replace(match[4], '');
 		var like = section.replace(/\//g,"-");
-		log.info("\n\nlike query = " + like + "\n" + match + " \n");
 		query = { id:['like', like + "%" ]  };
 		getPath = function(id){return match[1] + id.replace(like,'');};
 	}
 	
 	var allPads = sqlobj.selectMulti("PAD_SQLMETA",query);
-	// TODO - use a proper ejs template for this!
-	for(var i=0; i < allPads.length; i++){
-		var pad = allPads[i];
+	
+	var pads = allPads.map(function(pad){
 		var meta = sqlbase.getJSON("PAD_META", pad.id);
-		result += '<th><a href="' + getPath(pad.id) + '" title="'+pad.id+'">';
 		var image_available =   (meta && meta.images && meta.images.constructor && meta.images.constructor.toString().match(/Array/));
 		var src = image_available ? meta.images[0] : '/static/img/plugins/navigateByImageContent/placeholder.png'; 		
 		var width = image_available ? 60 : 24;
-		result += '<img src="' + src + '" alt="' + pad.id + '" width="' + width +'px"/>';
-		result += '</a></th>';
-	}
+		return {
+			pad: pad,
+			meta: meta,
+			image_available:image_available,
+			src:src,
+			width:width,
+			path:getPath(pad.id)
+		};
+				 
+	});
 	
-	return result + '</tr></table>';
+	return renderTemplateAsString('imageNavigation.ejs',{pads:pads, section:section}, 'navigateByImageContent');
 }
 navigateByImageContent = new init();
--- etherpad/src/plugins/supportGroupUrls/static/js/main.js
+++ etherpad/src/plugins/supportGroupUrls/static/js/main.js
@@ -4,29 +4,5 @@ import("etherpad.log");
 
 function init() {
   this.hooks = [];//['renderPageBodyPre'];
-  //this.aceGetFilterStack = renderPageBodyPre;
 }
-/*
-function renderPageBodyPre(){
-	var result = '<table><tr>';
-	
-	
-	var allPads = sqlobj.selectMulti("PAD_SQLMETA",{});
-	
-	for(var i=0; i < allPads.length; i++){
-		var pad = allPads[i];
-		var meta = sqlbase.getJSON("PAD_META", pad.id);
-		result += '<th><a href="/' + pad.id + '" title="'+pad.id+'">';
-		if (meta && meta.images && meta.images.constructor && meta.images.constructor.toString().match(/Array/)) {
-			
-			result += '<img src="' + meta.images[0] + '" alt="' + pad.id + '" width="60px"/>';
-		}
-		else {
-			result += pad.id;
-		}	
-		result += '</a></th>';
-	}
-	
-	return result + '</tr></table>';
-}*/
 supportGroupUrls = new init();
