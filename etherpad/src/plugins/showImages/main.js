6b4c4c8763d718eed623a99818d8ce0eae80432d
Egil Moeller <egil.moller@freecode.no>
2010-08-31 00:25:49 +0200

    Added the showImages plugin from Michael Forrest

---
+++ etherpad/src/plugins/showImages/main.js
@@ -0,0 +1,26 @@
+/*
+ * This plugin turns image paths into inline images in the document.
+ * Images must be hosted elsewhere.
+ *  Note that the same functionality has been hacked into exportHTML,
+ *  in a not-too-wonderful way. So you might want to watch out for that.
+ */
+import("etherpad.log");
+import("plugins.showImages.static.js.main");
+
+function showImagesInit() {
+ this.hooks = ['aceGetFilterStack', 'aceCreateDomLine'];
+ this.client = new main.showImagesInit(); 
+ this.description = 'Render images inline';
+ this.aceGetFilterStack = main.aceGetFilterStack;
+ this.aceCreateDomLine = main.aceCreateDomLine;
+ this.install = install;
+ this.uninstall = uninstall;
+}
+
+function install() {
+ log.info("Installing inlineImages");
+}
+
+function uninstall() {
+ log.info("Uninstalling inlineImages");
+}
