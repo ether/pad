ab1170455cc11175e2b2d99cff4548abafc48a91
root <root@sp.primarypad.com>
2010-10-09 19:03:34 +0100

    Check Scala, Java etc. is installed

--- bin/exports.sh
+++ bin/exports.sh
@@ -48,3 +48,23 @@ else
         export PATH="$JAVA_HOME/bin:$PATH"
 fi
 
+if ! [ -e "$MYSQL_CONNECTOR_JAR" ]; then
+        echo "MySql Connector jar '$MYSQL_CONNECTOR_JAR' not found - Download it here: http://dev.mysql.com/downloads/connector/j/3.1.html"
+        exit 1
+fi
+
+if ! [ -e "$SCALA_LIBRARY_JAR" ]; then
+        echo "Scala Library cannot be found '$SCALA_LIBRARY_JAR' not found - Download it here: http://www.scala-lang.org/"
+        exit 1
+fi
+
+if ! [ -e "$JAVA" ]; then
+        echo "Java cannot be found '$JAVA' not found - Download it here: http://openjdk.java.net/"
+        exit 1
+fi
+
+if ! [ -e "$SCALA" ]; then
+        echo "Java cannot be found '$SCALA' not found - Download it here: http://www.scala-lang.org/"
+        exit 1
+fi
+
