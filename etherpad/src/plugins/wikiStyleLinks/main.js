c0b9892cf2116f44929a37a76a6859195b66e3c3
Egil Moeller <egil.moller@freecode.no>
2010-10-10 18:47:15 +0200

    Search for pages that links to some page

--- etherpad/src/plugins/wikiStyleLinks/main.js
+++ etherpad/src/plugins/wikiStyleLinks/main.js
@@ -1,5 +1,7 @@
 import("etherpad.log");
 import("plugins.wikiStyleLinks.static.js.main");
+import("plugins.wikiStyleLinks.hooks");
+import("sqlbase.sqlobj");
 
 /* initialization function for the plugin */
 
@@ -20,11 +22,15 @@ import("plugins.wikiStyleLinks.static.js.main");
    establish the connection between client and server. */
 
 function wikiStyleLinksInit() {
- this.hooks = ['aceGetFilterStack', 'aceCreateDomLine'];
+ this.hooks = ['aceGetFilterStack', 'aceCreateDomLine', 'padModelWriteToDB', 'queryToSql', 'queryRefiner', 'querySummary'];
  this.client = new main.wikiStyleLinksInit();
  this.description = 'Wiki-style links lets the user link from Pad A to Pad B by writing [[name of Pad B]] in Pad A.';
  this.aceGetFilterStack = main.aceGetFilterStack;
  this.aceCreateDomLine = main.aceCreateDomLine;
+ this.padModelWriteToDB = hooks.padModelWriteToDB;
+ this.queryToSql = hooks.queryToSql;
+ this.querySummary = hooks.querySummary;
+ this.queryRefiner = hooks.queryRefiner;
 
  this.install = install;
  this.uninstall = uninstall;
@@ -36,6 +42,17 @@ function wikiStyleLinksInit() {
 
 function install() {
  log.info("Installing Wiki-style links");
+
+ sqlobj.createTable('PAD_LINK', {
+   PAD_ID: 'varchar(128) character set utf8 collate utf8_bin not null references PAD_META(ID)',
+   LINK: 'varchar(128) collate utf8_bin not null',
+  });
+
+ sqlobj.createTable('PAD_LINK_CACHE', {
+   PAD_ID: 'varchar(128) character set utf8 collate utf8_bin unique not null references PAD_META(ID)',
+   LINKS: 'varchar(1024) collate utf8_bin not null',
+  });
+
 }
 
 /* You probably don't want uninstalling to delete data.
