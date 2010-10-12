28e26d2145b1b8c1941dddfb7225c6736ec590b8
Egil Moeller <egil.moller@freecode.no>
2010-10-12 19:49:54 +0200

    Readonly link now works in pro mode

--- etherpad/src/plugins/linkReadOnly/hooks.js
+++ etherpad/src/plugins/linkReadOnly/hooks.js
@@ -1,10 +1,11 @@
 import("etherpad.log");
 import("etherpad.utils.*");
-import("etherpad.collab.server_utils.padIdToReadonly");
+import("etherpad.collab.server_utils");
+import("etherpad.pad.padutils");
 
 function docbarItemsPad() {
     var padId = request.path.split("/")[1];
     return ["<a href='/ep/pad/view/" +
-            padIdToReadonly(padId) +
+            server_utils.padIdToReadonly(padutils.getGlobalPadId(padId)) +
             "/latest'><img src='/static/img/plugins/linkReadOnly/glasses.gif'>Read-only Version</a>"];
 }
