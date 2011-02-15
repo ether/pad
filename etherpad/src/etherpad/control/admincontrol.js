bffa95446293271e758c9abc5111a650f2e2d669
Peter Martischka <petermartischka@googlemail.com>
2011-02-15 22:47:44 +0100

    Revert "Add an option to delete a pad via admin interface (thx @ Stefan Müller)"
    This reverts commit 08b529ee5ec1460638cebf7cc03b8b8100a75394.

--- etherpad/src/etherpad/control/admincontrol.js
+++ etherpad/src/etherpad/control/admincontrol.js
@@ -51,13 +51,15 @@ import("etherpad.collab.collab_server");
 import("etherpad.pro.pro_accounts");
 import("etherpad.pro.pro_utils");
 import("etherpad.pro.domains");
+import("etherpad.admin.plugins");
+import("etherpad.control.admin.pluginmanager");
 
 jimport("java.lang.System.out.println");
 
 jimport("net.appjet.oui.cometlatencies");
 jimport("net.appjet.oui.appstats");
+jimport("org.mindrot.BCrypt");
 
-import("etherpad.pad.dbwriter");
 
 //----------------------------------------------------------------
 
@@ -67,31 +69,6 @@ function _isAuthorizedAdmin() {
   }
   return (getSession().adminAuth === true);
 }
-
-var _mainLinks = [
-  ['exceptions', 'Exceptions Monitor'],
-  ['usagestats/', 'Usage Stats'],
-  ['padinspector', 'Pad Inspector'],
-  ['dashboard', 'Dashboard'],
-  ['eepnet-licenses', 'EEPNET Licenses'],
-  ['config', 'appjet.config'],
-  ['shell', 'Shell'],
-  ['timings', 'timing data'],
-  ['broadcast-message', 'Pad Broadcast'],
-//  ['analytics', 'Google Analytics'],
-  ['varz', 'varz'],
-  ['genlicense', 'Manually generate a license key'],
-  ['flows', 'Flows (warning: slow)'],
-  ['diagnostics', 'Pad Connection Diagnostics'],
-  ['cachebrowser', 'Cache Browser'],
-  ['pne-tracker', 'PNE Tracking Stats'],
-  ['reload-blog-db', 'Reload blog DB'],
-  ['pro-domain-accounts', 'Pro Domain Accounts'],
-  ['beta-valve', 'Beta Valve'],
-  ['reset-subscription', "Reset Subscription"],
-  ['delete-pad', "Delete Pad"],
-  ['flushpad', "Flushpad"]
-];
 
 function onRequest(name) {
   if (name == "auth") {
@@ -103,7 +80,11 @@ function onRequest(name) {
   }
 
   var disp = new Dispatcher();
+
+  disp.addLocations(plugins.callHook("handleAdminPath"));
+
   disp.addLocations([
+    [PrefixMatcher('/ep/admin/pluginmanager/'), forward(pluginmanager)],
     [PrefixMatcher('/ep/admin/usagestats/'), forward(statscontrol)]
   ]);
 
@@ -150,23 +131,11 @@ function render_auth() {
 }
 
 function render_main() {
-  var div = DIV();
-
-  div.push(A({href: "/"}, html("&laquo;"), " home"));
-  div.push(H1("Admin"));
-
-  _mainLinks.forEach(function(l) {
-    div.push(DIV(A({href: l[0]}, l[1])));
-  });
-  if (sessions.isAnEtherpadAdmin()) {
-    div.push(P(A({href: "/ep/admin/setadminmode?v=false"},
-                 "Exit Admin Mode")));
-  }
-  else {
-    div.push(P(A({href: "/ep/admin/setadminmode?v=true"},
-                 "Enter Admin Mode")));
-  }
-  response.write(HTML(_commonHead(), BODY(div)));
+  renderHtml("admin/page.ejs",
+   {
+    config: appjet.config,
+    bodyClass: 'nonpropad'
+   });
 }
 
 //----------------------------------------------------------------
@@ -180,10 +149,18 @@ function render_config() {
 
   vars.sort();
 
-  response.setContentType('text/plain; charset=utf-8');
+  body = PRE()
   vars.forEach(function(v) {
-    response.write("appjet.config."+v+" = "+appjet.config[v]+"\n");
+    body.push("appjet.config."+v+" = "+appjet.config[v]+"\n");
   });
+
+  renderHtml("admin/dynamic.ejs",
+   {
+    config: appjet.config,
+    bodyClass: 'nonpropad',
+    title: 'Config',
+    content: body
+   });
 }
 
 //----------------------------------------------------------------
@@ -200,7 +177,6 @@ function render_test() {
 
 function render_dashboard() {
   var body = BODY();
-  body.push(A({href: '/ep/admin/'}, html("&laquo; Admin")));
   body.push(H1({style: "border-bottom: 1px solid black;"}, "Dashboard"));
 
   /*
@@ -241,7 +217,13 @@ function render_dashboard() {
   body.push(H2({style: "color: #226; font-size: 1em;"}, "Recurring revenue, monthly"));
   body.push(renderRevenueStats());
 
-  response.write(HTML(_commonHead(), body));
+  renderHtml("admin/dynamic.ejs",
+   {
+    config: appjet.config,
+    bodyClass: 'nonpropad',
+    title: 'Dashboard',
+    content: body
+   });
 }
 
 // Note: This function is called by the PNE dashboard (pro_admin_control.js)!  Be careful.
@@ -377,13 +359,19 @@ function renderRevenueStats() {
 //----------------------------------------------------------------
 
 function render_broadcast_message_get() {
-  var body = BODY(FORM({action: request.path, method: 'post'},
-                       H3('Broadcast Message to All Active Pad Clients:'),
-                       TEXTAREA({name: 'msgtext', style: 'width: 100%; height: 100px;'}),
-                       H3('JavaScript code to be eval()ed on client (optional, be careful!): '),
-                       TEXTAREA({name: 'jscode', style: 'width: 100%; height: 100px;'}),
-                       INPUT({type: 'submit', value: 'Broadcast Now'})));
-  response.write(HTML(body));
+  var body = FORM({action: request.path, method: 'post'},
+		  H3('Broadcast Message to All Active Pad Clients:'),
+		  TEXTAREA({name: 'msgtext', style: 'width: 100%; height: 100px;'}),
+		  H3('JavaScript code to be eval()ed on client (optional, be careful!): '),
+		  TEXTAREA({name: 'jscode', style: 'width: 100%; height: 100px;'}),
+		  INPUT({type: 'submit', value: 'Broadcast Now'}));
+  renderHtml("admin/dynamic.ejs",
+   {
+    config: appjet.config,
+    bodyClass: 'nonpropad',
+    title: 'Broadcast message',
+    content: body
+   });
 }
 
 function render_broadcast_message_post() {
@@ -399,7 +387,13 @@ function render_broadcast_message_post() {
     text: msgText,
     js: jsCode
   });
-  response.write(HTML(BODY(P("OK"), P(A({href: request.path}, "back")))));
+  renderHtml("admin/dynamic.ejs",
+   {
+    config: appjet.config,
+    bodyClass: 'nonpropad',
+    title: 'Broadcast message',
+    content: P("OK")
+   });
 }
 
 function render_shell() {
@@ -534,9 +528,13 @@ function render_padinspector_get() {
       '});'
     ].join('\n')));
 
-    response.write(HTML(
-      HEAD(SCRIPT({type: 'text/javascript', src: '/static/js/jquery-1.3.2.js?'+(+(new Date))})),
-      BODY(div, script)));
+    renderHtml("admin/dynamic.ejs",
+     {
+      config: appjet.config,
+      bodyClass: 'nonpropad',
+      title: 'Pad inspector',
+      content: DIV(div, script)
+     });
   }, "r");
 }
 
@@ -617,13 +615,13 @@ function render_appendtest() {
 //  response.write("OK");
 //}
 
-  function render_flushpad() {
-    var padId = request.params.padId;
-    model.accessPadGlobal(padId, function(pad) {
-      dbwriter.writePad(pad, true);
-    });
-    response.write("OK");
- }
+//function render_flushpad() {
+//  var padId = request.params.padId;
+//  model.accessPadGlobal(padId, function(pad) {
+//    dbwriter.writePad(pad, true);
+//  });
+//  response.write("OK");
+//}
 
 /*function render_foo() {
   locking.doWithPadLock("CAT", function() {
@@ -656,7 +654,8 @@ function render_timings() {
   var timer = Packages.net.appjet.ajstdlib.timer;
   var opnames = timer.getOpNames();
 
-  response.write(P(A({href: '/ep/admin/timingsreset'}, "reset all")));
+  var d = DIV()
+  d.push(P(A({href: '/ep/admin/timingsreset'}, "reset all")));
 
   var t = TABLE({border: 1, cellspacing: 0, cellpadding: 3, style: 'font-family: monospace;'});
   t.push(TR(TH("operation"),
@@ -686,8 +685,14 @@ function render_timings() {
               TD(r(row[2])),
               TD(r(row[3]))));
   });
-
-  response.write(t);
+  d.push(t);
+  renderHtml("admin/dynamic.ejs",
+   {
+    config: appjet.config,
+    bodyClass: 'nonpropad',
+    title: 'Timing data',
+    content: d
+   });
 }
 
 function render_timingsreset() {
@@ -762,10 +767,19 @@ function render_timingsreset() {
 
 function render_varz() {
   var varzes = varz.getSnapshot();
-  response.setContentType('text/plain; charset=utf-8');
+
+  var body = PRE();
   for (var k in varzes) {
-    response.write(k+': '+varzes[k]+'\n');
+    body.push(k+': '+varzes[k]+'\n');
   }
+
+  renderHtml("admin/dynamic.ejs",
+   {
+    config: appjet.config,
+    bodyClass: 'nonpropad',
+    title: 'Varz',
+    content: body
+   });
 }
 
 function render_extest() {
@@ -844,13 +858,19 @@ function render_diagnostics() {
     t.push(tr);
   });
 
-  var body = BODY();
+  var body = DIV();
   body.push(P("Showing entries ", start, "-", start+diagnostic_entries.length, ". ",
               (start > 0 ? makeLink("Show previous "+count+".", [], [], start-count) : ""),
               (diagnostic_entries.length == count ? makeLink("Show next "+count+".", [], [], start+count) : "")));
   body.push(t);
 
-  response.write(HTML(body));
+  renderHtml("admin/dynamic.ejs",
+   {
+    config: appjet.config,
+    bodyClass: 'nonpropad',
+    title: 'Diagnostics',
+    content: body
+   });
 }
 
 //----------------------------------------------------------------
@@ -874,7 +894,7 @@ function render_testbillingdirect() {
     addressState: "NY",
     addressCountry: "US",
     addressZip: "10044"
-  }, "http://"+request.host.split(':')[0]+"/ep/about/testbillingnotify");
+  }, "https://"+request.host+"/ep/about/testbillingnotify");
   if (ret.status == 'success') {
     response.write(P("Success! Invoice id: "+ret.purchaseInfo.invoiceId+" for "+ret.purchaseInfo.cost));
   } else {
@@ -900,7 +920,7 @@ function render_testbillingrecurring() {
     addressState: "NY",
     addressCountry: "US",
     addressZip: "10044"
-  }, "http://"+request.host.split(':')[0]+"/ep/about/testbillingnotify", true);
+  }, "https://"+request.host+"/ep/about/testbillingnotify", true);
   if (ret.status == 'success') {
     var transactionId = billing.getTransaction(ret.purchaseInfo.transactionId).txnId;
     var purchaseId = ret.purchaseInfo.purchaseId;
@@ -909,7 +929,7 @@ function render_testbillingrecurring() {
     invoiceId = billing.createInvoice();
     ret = billing.asyncRecurringPurchase(
       invoiceId, purchaseId, transactionId, 500,
-      "https://"+request.host.split(':')[0]+"/ep/about/testbillingnotify");
+      "https://"+request.host+"/ep/about/testbillingnotify");
     if (ret.status == 'success') {
       response.write(P("Woot! Recurrent billing successful! ", ret.purchaseInfo.invoiceId, " for ", ret.purchaseInfo.cost));
     } else {
@@ -921,9 +941,9 @@ function render_testbillingrecurring() {
 }
 
 function render_testbillingexpress() {
-  var urlPrefix = "http://"+request.host.split(':')[0]+request.path;
+  var urlPrefix = "http://"+request.host+request.path;
   var session = sessions.getSession();
-  var notifyUrl = "http://"+request.host.split(':')[0]+"/ep/about/testbillingnotify";
+  var notifyUrl = "http://"+request.host+"/ep/about/testbillingnotify";
 
   switch (request.params.step) {
     case '0':
@@ -1273,7 +1293,14 @@ function render_reload_blog_db_get() {
   }
   d.push(FORM({method: "post", action: request.path},
     INPUT({type: "submit", value: "Reload Blog DB Now"})));
-  response.write(HTML(BODY(d)));
+
+  renderHtml("admin/dynamic.ejs",
+   {
+    config: appjet.config,
+    bodyClass: 'nonpropad',
+    title: 'Reload blog DB',
+    content: d
+   });
 }
 
 function render_reload_blog_db_post() {
@@ -1285,26 +1312,46 @@ function render_pro_domain_accounts() {
   var accounts = sqlobj.selectMulti('pro_accounts', {}, {});
   var domains = sqlobj.selectMulti('pro_domains', {}, {});
 
+  if (request.method == "POST") {
+    accounts.forEach(function(u) {
+      var pwd = request.params["password_" + u.email];
+      if (pwd != undefined && pwd != '') {
+        /* Ugly hack since we're not on a domain, so we can't really use pro_domains.*  functions */
+	sqlobj.update('pro_accounts', {id: u.id}, {passwordHash: BCrypt.hashpw(pwd, BCrypt.gensalt(10))});
+      }
+    });
+  }
+
   // build domain map
   var domainMap = {};
   domains.forEach(function(d) { domainMap[d.id] = d; });
   accounts.sort(function(a,b) { return cmp(b.lastLoginDate, a.lastLoginDate); });
 
-  var b = BODY({style: "font-family: monospace;"});
+  var b = DIV({style: "font-family: monospace;"});
   b.push(accounts.length + " pro accounts.");
   var t = TABLE({border: 1});
   t.push(TR(TH("email"),
             TH("domain"),
-            TH("lastLogin")));
+            TH("lastLogin"),
+	    TH("password")));
   accounts.forEach(function(u) {
     t.push(TR(TD(u.email),
               TD(domainMap[u.domainId].subDomain+"."+request.domain),
-              TD(u.lastLoginDate)));
+              TD(u.lastLoginDate),
+	      TD(INPUT({type: "password", name: "password_" + u.email}))));
   });
 
   b.push(t);
+  b.push(INPUT({type: "submit", value: "Save"}));
 
-  response.write(HTML(b));
+  renderHtml("admin/dynamic.ejs",
+   {
+    config: appjet.config,
+    bodyClass: 'nonpropad',
+    title: 'Pro accounts',
+    content: FORM({method: "POST", action: request.path}, b)
+   });
+
 }
 
 
@@ -1357,7 +1404,14 @@ function render_beta_valve_get() {
     t.push(tr);
   });
   d.push(t);
-  response.write(d);
+
+  renderHtml("admin/dynamic.ejs",
+   {
+    config: appjet.config,
+    bodyClass: 'nonpropad',
+    title: 'Beta valve',
+    content: d
+   });
 }
 
 function render_beta_valve_toggle_post() {
@@ -1435,7 +1489,7 @@ function render_runsubscriptions() {
 }
 
 function render_reset_subscription() {
-  var body = BODY();
+  var body = DIV();
   if (request.isGet) {
     body.push(FORM({method: "POST"},
                    "Subdomain: ", INPUT({type: "text", name: "subdomain"}), BUTTON({name: "clear"}, "Go")));
@@ -1469,24 +1523,11 @@ function render_reset_subscription() {
       body.push("Done!")
     }
   }
-  body.push(A({href: request.path}, html("&laquo; back")));
-  response.write(HTML(body));
-}
-
-function render_delete_pad() {
-  var body = BODY();
-  response.write("Delete Pad:");
-  if (request.isGet) {
-    body.push(FORM({method: "POST"},
-                   "padId: ", INPUT({type: "text", name: "padId"}), BUTTON({name: "delete"}, "Delete")));
-  } else if (request.isPost) {
-    var localPadId = request.params.padId;
-    model.accessPadGlobal(localPadId, function(pad) {
-            collab_server.bootUsersFromPad(pad, "deleted");
-            pad.destroy();
-    });
-    dbwriter.taskFlushPad(localPadId, "delete");
-  }
-  body.push(A({href: request.path}, html("&laquo; back")));
-  response.write(HTML(body));
+  renderHtml("admin/dynamic.ejs",
+   {
+    config: appjet.config,
+    bodyClass: 'nonpropad',
+    title: 'Reset subscription',
+    content: body
+   });
 }
