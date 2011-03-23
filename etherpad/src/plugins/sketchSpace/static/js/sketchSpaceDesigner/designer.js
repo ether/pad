41ed24a9978beee731f5a90e595994614f92150e
Egil Moeller <egil.moller@freecode.no>
2011-03-23 13:31:56 +0100

    More code cleanup

--- etherpad/src/plugins/sketchSpace/static/js/sketchSpaceDesigner/designer.js
+++ etherpad/src/plugins/sketchSpace/static/js/sketchSpaceDesigner/designer.js
@@ -16,7 +16,7 @@ dojo.declare("sketchSpaceDesigner.designer.Designer", [], {
 
     this.images = {};
     this.currentImage = undefined;
-    this.selection = new sketchSpaceDesigner.designer.selection.Selection();
+    this.selection = new sketchSpaceDesigner.designer.selection.Selection(this);
 
     dojo.connect(this.container, "ondragstart",   dojo, "stopEvent");
     dojo.connect(this.container, "onselectstart", dojo, "stopEvent");
@@ -57,7 +57,7 @@ dojo.declare("sketchSpaceDesigner.designer.Designer", [], {
   },
 
   editorCallbackShapeClick: function(shape, event) {
-    this.editorShapeToggleSelection(shape, !event.ctrlKey);
+    this.selection.editorShapeToggleSelection(shape, !event.ctrlKey);
   },
 
   editorShapeRemove: function(shape) {
@@ -76,78 +76,6 @@ dojo.declare("sketchSpaceDesigner.designer.Designer", [], {
   editorAddCircle: function() {
     this.editorAddShape({parent:null,shape:{"shape":{"type":"circle","cx":100,"cy":100,"r":50},"stroke":{"type":"stroke","color":{"r":0,"g":255,"b":0,"a":1},"style":"solid","width":2,"cap":"butt","join":4},"fill":{"r":255,"g":0,"b":0,"a":1}}});
   },
-
-  editorSelectionBbox: function() {
-    var bbox = new sketchSpaceDesigner.designer.bbox.Bbox();
-    for (objId in this.selection.objects) {
-      bbox.addPoints(this.selection.objects[objId].getTransformedBoundingBox());
-    }
-    return bbox;
-  },
-
-  editorSelectionUpdateOutline: function() {
-    var bbox = this.editorSelectionBbox();
-
-    if (this.selection.outline !== undefined) {
-      this.selection.outline.removeShape();
-      this.selection.outline = undefined;
-    }
-
-    if (bbox.x !== undefined) {
-      this.selection.outline = this.surface.createGroup();
-
-      this.selection.outline.setTransform(dojox.gfx.matrix.translate(bbox.x, bbox.y));
-
-      this.selection.outline.outlineRect = dojox.gfx.utils.deserialize(this.selection.outline, {shape:{type:"rect", x:0, y:0, width:bbox.width, height:bbox.height}, stroke:{color:{r:196,g:196,b:196,a:1},width:1, style:"solid"}});
-
-      this.selection.outline.outlineCornerTL = dojox.gfx.utils.deserialize(this.selection.outline, {shape:{type:"rect", x:-2, y:-2, width:4, height:4}, stroke:{color:{r:128,g:128,b:128,a:1},width:1}, fill:{r:196,g:196,b:196,a:1}});
-      this.selection.outline.outlineCornerBL = dojox.gfx.utils.deserialize(this.selection.outline, {shape:{type:"rect", x:-2, y:bbox.height-2, width:4, height:4}, stroke:{color:{r:128,g:128,b:128,a:1},width:1}, fill:{r:196,g:196,b:196,a:1}});
-      this.selection.outline.outlineCornerTH = dojox.gfx.utils.deserialize(this.selection.outline, {shape:{type:"rect", x:bbox.width-2, y:-2, width:4, height:4}, stroke:{color:{r:128,g:128,b:128,a:1},width:1}, fill:{r:196,g:196,b:196,a:1}});
-      this.selection.outline.outlineCornerBH = dojox.gfx.utils.deserialize(this.selection.outline, {shape:{type:"rect", x:bbox.width-2, y:bbox.height-2, width:4, height:4}, stroke:{color:{r:128,g:128,b:128,a:1},width:1}, fill:{r:196,g:196,b:196,a:1}});
-    }
-  },
-
-  editorShapeAddToSelection: function(shape) {
-    if (shape.objId === undefined) return;
-    if (this.selection.parent !== shape.parent) {
-      this.selection.objects = {};
-      this.selection.parent = shape.parent;
-    }
-    this.selection.objects[shape.objId] = shape;
-    this.editorSelectionUpdateOutline();
-  },
-
-  editorShapeIsSelected: function(shape) {
-    return shape.objId !== undefined && this.selection.objects[shape.objId] !== undefined;
-  },
-
-  editorShapeRemoveFromSelection: function(shape) {
-    if (shape.objId === undefined || this.selection.objects[shape.objId] === undefined) return;
-    delete this.selection.objects[shape.objId];
-    this.editorSelectionUpdateOutline();
-  },
-
-  editorShapeToggleSelection: function(shape, clearOthers) {
-    var isSelected = this.editorShapeIsSelected(shape);
-    if (clearOthers)
-      this.editorShapeClearSelection();
-    if (isSelected)
-      this.editorShapeRemoveFromSelection(shape);
-    else
-      this.editorShapeAddToSelection(shape);
-  },
-
-  editorShapeClearSelection: function () {
-    this.selection.objects = {};
-    this.editorSelectionUpdateOutline();
-  },
-
-  editorSelectionShapeRemove: function() {
-    for (objId in this.selection.objects) {
-      this.editorShapeRemove(this.selection.objects[objId]);
-    }
-    this.editorShapeClearSelection();
-  }
 
 
 });
--- etherpad/src/plugins/sketchSpace/static/js/sketchSpaceDesigner/designer/selection.js
+++ etherpad/src/plugins/sketchSpace/static/js/sketchSpaceDesigner/designer/selection.js
@@ -1,10 +1,84 @@
 dojo.provide("sketchSpaceDesigner.designer.selection");
 
+dojo.require("sketchSpaceDesigner.designer.bbox");
+
 dojo.declare("sketchSpaceDesigner.designer.selection.Selection", [], {
   constructor: function (designer) {
     this.designer = designer;
     this.objects = {};
     this.parent = undefined;
     this.outline = undefined;
+  },
+
+  editorSelectionBbox: function() {
+    var bbox = new sketchSpaceDesigner.designer.bbox.Bbox();
+    for (objId in this.objects) {
+      bbox.addPoints(this.objects[objId].getTransformedBoundingBox());
+    }
+    return bbox;
+  },
+
+  editorSelectionUpdateOutline: function() {
+    var bbox = this.editorSelectionBbox();
+
+    if (this.outline !== undefined) {
+      this.outline.removeShape();
+      this.outline = undefined;
+    }
+
+    if (bbox.x !== undefined) {
+      this.outline = this.designer.surface.createGroup();
+
+      this.outline.setTransform(dojox.gfx.matrix.translate(bbox.x, bbox.y));
+
+      this.outline.outlineRect = dojox.gfx.utils.deserialize(this.outline, {shape:{type:"rect", x:0, y:0, width:bbox.width, height:bbox.height}, stroke:{color:{r:196,g:196,b:196,a:1},width:1, style:"solid"}});
+
+      this.outline.outlineCornerTL = dojox.gfx.utils.deserialize(this.outline, {shape:{type:"rect", x:-2, y:-2, width:4, height:4}, stroke:{color:{r:128,g:128,b:128,a:1},width:1}, fill:{r:196,g:196,b:196,a:1}});
+      this.outline.outlineCornerBL = dojox.gfx.utils.deserialize(this.outline, {shape:{type:"rect", x:-2, y:bbox.height-2, width:4, height:4}, stroke:{color:{r:128,g:128,b:128,a:1},width:1}, fill:{r:196,g:196,b:196,a:1}});
+      this.outline.outlineCornerTH = dojox.gfx.utils.deserialize(this.outline, {shape:{type:"rect", x:bbox.width-2, y:-2, width:4, height:4}, stroke:{color:{r:128,g:128,b:128,a:1},width:1}, fill:{r:196,g:196,b:196,a:1}});
+      this.outline.outlineCornerBH = dojox.gfx.utils.deserialize(this.outline, {shape:{type:"rect", x:bbox.width-2, y:bbox.height-2, width:4, height:4}, stroke:{color:{r:128,g:128,b:128,a:1},width:1}, fill:{r:196,g:196,b:196,a:1}});
+    }
+  },
+
+  editorShapeAddToSelection: function(shape) {
+    if (shape.objId === undefined) return;
+    if (this.parent !== shape.parent) {
+      this.objects = {};
+      this.parent = shape.parent;
+    }
+    this.objects[shape.objId] = shape;
+    this.editorSelectionUpdateOutline();
+  },
+
+  editorShapeIsSelected: function(shape) {
+    return shape.objId !== undefined && this.objects[shape.objId] !== undefined;
+  },
+
+  editorShapeRemoveFromSelection: function(shape) {
+    if (shape.objId === undefined || this.objects[shape.objId] === undefined) return;
+    delete this.objects[shape.objId];
+    this.editorSelectionUpdateOutline();
+  },
+
+  editorShapeToggleSelection: function(shape, clearOthers) {
+    var isSelected = this.editorShapeIsSelected(shape);
+    if (clearOthers)
+      this.editorShapeClearSelection();
+    if (isSelected)
+      this.editorShapeRemoveFromSelection(shape);
+    else
+      this.editorShapeAddToSelection(shape);
+  },
+
+  editorShapeClearSelection: function () {
+    this.objects = {};
+    this.editorSelectionUpdateOutline();
+  },
+
+  editorSelectionShapeRemove: function() {
+    for (objId in this.objects) {
+      this.designer.editorShapeRemove(this.objects[objId]);
+    }
+    this.editorShapeClearSelection();
   }
 });
