6c74eb1ea3b936d26d4515f175a0b5664efc9a87
Egil Moeller <egil.moller@freecode.no>
2011-04-28 13:23:11 +0200

    More OD styling

--- etherpad/src/plugins/openingDesign/hooks.js
+++ etherpad/src/plugins/openingDesign/hooks.js
@@ -7,7 +7,8 @@ import("etherpad.helpers");
 
 function docbarItemsPad() {
     return ['<a href="javascript:sketchSpace.insertImage()" title="SketchSpace"><img src="/static/html/plugins/sketchSpace/editbar_icon.png">New blank SketchSpace</a>',
-	    '<a href="javascript:void(0)" title="SketchSpace upload PDF" class="sketchSpaceAddPdfImage"><img src="/static/html/plugins/sketchSpace/editbar_pdf_icon.png">New SketchSpace(s) from PDF</a>'];
+	    '<a href="javascript:void(0)" title="SketchSpace upload PDF" class="sketchSpaceAddPdfImage"><img src="/static/html/plugins/sketchSpace/editbar_pdf_icon.png">New SketchSpace(s) from PDF</a>',
+	    '<a href="javascript:void(0)" title="Share answer with...">Share answer with...</a>',];
 }
 
 function modals(arg) {
