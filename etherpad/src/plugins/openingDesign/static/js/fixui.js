57c1316e371eeb2d1541bea7246a203cb8e77b73
Egil Moeller <egil.moller@freecode.no>
2011-05-13 09:17:02 +0200

    Removed some customer-specific cludges

--- etherpad/src/plugins/openingDesign/static/js/fixui.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  var e = jQuery.Event('mousedown');
-  e.pageX = 0; $('#vdraggie').trigger(e);
-  e = jQuery.Event('mouseup');
-  e.pageX = 0; $('#vdraggie').trigger(e);
-});
