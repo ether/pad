73c277da77aabf1750c35bf44858823383f27bf5
XM <tengyaogao@gmail.com>
2011-08-20 23:47:26 +0800

    hook for export inline style

--- etherpad/src/plugins/richText/main.js
+++ etherpad/src/plugins/richText/main.js
@@ -1,9 +1,11 @@
 import("etherpad.log");
 import("plugins.richText.hooks");
+import("plugins.richText.importexport");
 import("plugins.richText.static.js.main");
 
 function richTextInit() {
-    this.hooks = [ 'editBarItemsLeftPad', 'editBarItemsLeftPadView', 'aceAttribsToClasses', 'aceCreateDomLine', 'aceCreateStructDomLine'];
+    this.hooks = ['editBarItemsLeftPad', 'editBarItemsLeftPadView', 'aceAttribsToClasses', 
+                  'aceCreateDomLine', 'aceCreateStructDomLine', 'exportInlineStyle'];
     this.description = 'Rich Text editing in etherpad';
     this.client = new main.richTextInit();
     this.editBarItemsLeftPad = hooks.editBarItemsLeftPad;
@@ -11,6 +13,7 @@ function richTextInit() {
     this.aceAttribsToClasses = main.aceAttribsToClasses;
     this.aceCreateDomLine = main.aceCreateDomLine;
     this.aceCreateStructDomLine = main.aceCreateStructDomLine; 
+    this.exportInlineStyle = importexport.exportInlineStyle;
     this.install = install;
     this.uninstall = uninstall;
 }
